011c8e5a2aa0318d79f98b49bc4cc83c0f9f2994
Jack Dalton <jackdalton@users.noreply.github.com>
2014-12-20 17:58:27 -0700

    Comments added
    Comments added to explain as many functions and variables as possible, to make code legible.

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-// Main Falling Squares game script
-$(document).ready(init);
+// main Falling Squares game script
+$(document).ready(init); // functions init
 var x = 240,
     y = 450,
     c, ctx, coep,
@@ -12,8 +12,8 @@ var x = 240,
     ammo = true,
     then = Date.now(),
     lost = false,
-    middle;
-var fps = {
+    middle; // misc vars
+var fps = { // fps counter
     startTime: 0,
     frameNumber: 0,
     getFPS: function() {
@@ -31,7 +31,7 @@ var fps = {
     }
 };
 
-function init() {
+function init() { // main init function
     c = document.getElementById("canvas");
     ctx = c.getContext("2d");
     gameLoop();
@@ -40,17 +40,17 @@ function init() {
     setTimeout(hideInst, 7500);
     middle = ($(window).width() / 2) - 125;
 }
-function hideInst() {
+function hideInst() { // instructions fade out
     $("#inst").fadeTo("slow", 0);
 }
-function scoreLoop() {
+function scoreLoop() { // score loop
     score++;
     setTimeout(scoreLoop, 250);
 }
-function reset() {
+function reset() { // reset button
     window.location.assign("http://html5.jackdalton.co");
 }
-function lose() {
+function lose() { // loss function
     $("#reset").fadeTo("slow", 1);
     lost = true;
     //window.location.assign("#lose");
@@ -60,63 +60,61 @@ function lose() {
     slideLoss();
 }
 
-function slideLoss() {
+function slideLoss() { // loss message display
     $("#lossmessage").animate({
         left: middle
     }, 250);
 }
-function gameLoop() {
-    c.width = c.width;
-    if (lost === false) {
-        for (var i = 0; i <= obs.length; i++) {
+function gameLoop() { // main game loop
+    c.width = c.width; // clears canvas
+    if (!lost) {
+        for (var i = 0; i <= obs.length; i++) { // render obstacles
             ctx.fillRect(obs[i], obsy[i], 10, 10);
         }
-        var collision_space = ctx.getImageData(x, y, 5, 5).data;
-        for (var pixel in collision_space) {
+        var collision_space = ctx.getImageData(x, y, 5, 5).data; // checks for collision
+        for (var pixel in collision_space) { // checks for collision
             if (collision_space[pixel] !== 0) {
                 lose();
                 break;
             }
         }
-        ctx.fillStyle = "red";
-        ctx.fillRect(x, y, 5, 5);
-        ctx.fillText("Score: " + score, 10, 10);
-        ctx.fillText(fps.getFPS() + " FPS", 450, 20);
-        coep = Math.random() * 10 + 1;
-        coep = Math.round(coep);
-        if (coep < 5) {
+        ctx.fillStyle = "red"; // sets color
+        ctx.fillRect(x, y, 5, 5); // render player
+        ctx.fillText("Score: " + score, 10, 10); // render score
+        ctx.fillText(fps.getFPS() + " FPS", 450, 20); // render FPS
+        coep = Math.random() * 10 + 1; // calculates chance of obstacle spawn
+        coep = Math.round(coep); // rounds calculation
+        if (coep < 5) { // determines whether or not to spawn obstacle
             spawnE();
         }
-        var now = Date.now();
+        var now = Date.now(); // time deltas for movement
         var delta = now - then;
         move(delta / 1000);
         then = now;
-        window.requestAnimationFrame(gameLoop);
+        window.requestAnimationFrame(gameLoop); // starts it all over again
     }
 }
 
-function downLoop() {
-    for (var i = 0; i <= obs.length; i++) {
-        if (obsy[i] < 500) {
+function downLoop() { // obstacle descent loop
+    for (var i = 0; i <= obs.length; i++) { // iterates through each obstacle
+        if (obsy[i] < 500) { // determines whether or not obstacle is displayed, drop it
             //obsy[i] += 1.5;
-            obsy[i] += 2;
+            obsy[i] += 2; // drops obstacle
         }
     }
-    setTimeout(downLoop, 10);
+    setTimeout(downLoop, 10); // starts it all over again
 }
 
-var move = function(m) {
-    if (37 in keysDown && x > 10) {
-        // Left
+var move = function(m) { // movement function
+    if (37 in keysDown && x > 10) { // if left arrow key pressed, move left
         x -= 180 * m;
     }
-    if (39 in keysDown && x < 485) {
-        // Right
+    if (39 in keysDown && x < 485) { // if right arrow key pressed, move right
         x += 180 * m;
     }
 };
-
-function outOfAmmo() {
+//****************Old gun function (removed because it changes the objective too drastically)***************
+/*function outOfAmmo() {
     document.getElementById("ammo").innerHTML = "Out of ammo.";
 }
 
@@ -146,19 +144,19 @@ function shoot() {
     sx = x;
     sy = 450;
     window.requestAnimationFrame(doShot);
-}
-addEventListener("keydown", function(e) {
+}*/
+addEventListener("keydown", function(e) { // checks for keys down
     keysDown[e.keyCode] = true;
 }, false);
 
-addEventListener("keyup", function(e) {
+addEventListener("keyup", function(e) { // checks for keys released
     delete keysDown[e.keyCode];
 }, false);
 var obs = [];
 var randX;
 var keysDown = {};
 
-function spawnE() {
+function spawnE() { // spawns obstacle
     randX = Math.round(Math.random() * 494 + 1);
     obs[obs.length] = randX;
     obsy[obsy.length] = 5;
